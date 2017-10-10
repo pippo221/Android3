3cf10b093943f5c53963b65f01560b5313359336
cuongducnguyen.kp <lumo.aq@gmail.com>
2017-10-10 14:23:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cuongducnguyenkp/myapplication/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.cuongducnguyenkp.myapplication;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends Activity {
+    Button b1,b2;
+    EditText ed1,ed2;
+    private ListView lv;
+    private ArrayAdapter<String> adapter;
+    private EditText inputSearch;
+    private ArrayList<HashMap<String, String>> productList;
+
+    TextView tx1;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        b1 = (Button)findViewById(R.id.button);
+        ed1 = (EditText)findViewById(R.id.editText);
+        ed2 = (EditText)findViewById(R.id.editText2);
+
+        b2 = (Button)findViewById(R.id.button2);
+        tx1 = (TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                    Intent in=new Intent(MainActivity.this,second_main.class);
+                    startActivity(in);
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...2",Toast.LENGTH_SHORT).show();
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                            tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/cuongducnguyenkp/myapplication/second_main.java
@@ -0,0 +1,58 @@
+package com.example.cuongducnguyenkp.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+/**
+ * Created by cuongducnguyen.kp on 10/10/2017.
+ */
+
+public class second_main extends Activity {
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private EditText editTextInputSearch;
+    private ArrayList<HashMap<String, String>> arrayListProductList;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        String products[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S", "HTC Sense", "HTC Sensation XE",
+                "iPhone 4S", "Samsung Galaxy Note 800",
+                "Samsung Galaxy S3", "MacBook Air", "Mac Mini", "MacBook Pro"};
+        listView = (ListView) findViewById(R.id.list_view);
+        editTextInputSearch = (EditText) findViewById(R.id.inputSearch);
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.product_name, products);
+        listView.setAdapter(arrayAdapter);
+        editTextInputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                second_main.this.arrayAdapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+
+}
